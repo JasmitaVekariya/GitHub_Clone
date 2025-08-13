9a2f062b76d39bc93673ad4684936ce1d6a1d2c8
Jasmita Vekariya <pjasmita7@gmail.com>
2025-08-13 15:43:22 +0530

    login signup ui creation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { AuthProvider } from './authContext.jsx'  
+// import ProjectRoutes from './Routes.jsx'
+// import { BrowserRouter as Router} from 'react-router-dom'
+// createRoot(document.getElementById('root')).render(
+//   <AuthProvider>
+//   <Router>
+//     <ProjectRoutes />
+//     </Router>
+//   </AuthProvider>,
+// )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './authContext.jsx'  
-import ProjectRoutes from './Routes.jsx'
-import { BrowserRouter as Router} from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
+import './index.css'
+import { AuthProvider } from './authContext.jsx'
+import ProjectRoutes from './Routes.jsx';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-  <Router>
-    <ProjectRoutes />
+    <Router>
+      <ProjectRoutes />
     </Router>
-  </AuthProvider>,
-)
+  </AuthProvider>
+);
