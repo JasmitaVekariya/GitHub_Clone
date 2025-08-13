d528e8328dae0012f77fc16627f3dd4ab7b22e04
Archii1201 <1201archii@gmail.com>
2025-08-13 15:50:53 +0530

    changes

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,50 +1,56 @@
-import React,{useEffect} from "react";
-import {useNavigate,useRoutes } from "react-router-dom";
-//pagelist
+import React, { useEffect } from "react";
+import {useNavigate, useRoutes} from 'react-router-dom'
+
+// Pages List
 import Dashboard from "./components/dashboard/Dashboard";
+import Profile from "./components/user/Profile";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
-import Profile from "./components/user/Profile";
 
-//auht context
+// Auth Context
 import { useAuth } from "./authContext";
-const ProjectRoutes = () => {
-    const { currentUser,setUser } = useAuth();
+
+const ProjectRoutes = ()=>{
+    const {currentUser, setCurrentUser} = useAuth();
     const navigate = useNavigate();
-    useEffect(() => {
-     const userIdFromStorage = localStorage.getItem("userId");   
-     if (userIdFromStorage&&!currentUser) {
-         setCurrentUser(userIdFromStorage );
-     } 
-    if (!userIdFromStorage&&!["/auth", "/signup"].includes(window.location.pathname)) {
+
+    useEffect(()=>{
+        const userIdFromStorage = localStorage.getItem("userId");
+
+        if(userIdFromStorage && !currentUser){
+            setCurrentUser(userIdFromStorage);
+        }
+
+        if(!userIdFromStorage && !["/auth", "/signup"].includes(window.location.pathname))
+        {
             navigate("/auth");
-    }
-    if( userIdFromStorage && window.location.pathname=='/auth') {
+        }
+
+        if(userIdFromStorage && window.location.pathname=='/auth'){
             navigate("/");
         }
     }, [currentUser, navigate, setCurrentUser]);
-   let elemet = useRoutes([
-    {
-        path: "/",
-        element: <Dashboard />,
-        
-    },
-      {
-        path: "/auth",
-        element: <Login/>,
-        
-    },
-     {
-        path: "/singup",
-        element: <Signup/>,
-        
-    },
-     {
-        path: "/profile",
-        element: <Profile/>,
-        
-    }
-   ]);
-   return elemet;
+
+    let element = useRoutes([
+        {
+            path:"/",
+            element:<Dashboard/>
+        },
+        {
+            path:"/auth",
+            element:<Login/>
+        },
+        {
+            path:"/signup",
+            element:<Signup/>
+        },
+        {
+            path:"/profile",
+            element:<Profile/>
+        }
+    ]);
+
+    return element;
 }
+
 export default ProjectRoutes;
