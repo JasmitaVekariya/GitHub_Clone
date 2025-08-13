4107a6f2a6f1bdf7049033c279703da11ceda71d
Archii1201 <1201archii@gmail.com>
2025-08-13 15:42:45 +0530

    dashborad1

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React ,{useState ,useEffect}from 'react';
 
 const Dashboard = async () => {
-
+    const [repositoires, setRepositories] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    useEffect(() => {
+       const userId = localStorage.getItem('userId');
+       const fetchRepositories = async () => {
+           try {
+               const response = await fetch(`http://localhost:3002/repo/user/${userId}`);
+               if (!response.ok) {
+                   throw new Error('Failed to fetch repositories');
+               }
+               const data = await response.json();
+               setRepositories(data);
+           } catch (error) {
+               console.error('Error fetching repositories:', error);
+           }
+       }
+          
+    }, []);
 }
 
 export default Dashboard;
